a851ab23593d73a3d221ce295de2017f8fc9225b
Vitaliy Rogov <vv55rogov@gmail.com>
2023-09-03 23:49:02 +0600

    initial commit

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CallbackTest {
+    private String makeDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[data-test-id=callback-form]");
+        form.$("[data-test-id=name] input").setValue("Васильев Василий");
+        form.$("[data-test-id=phone] input").setValue("+7927123456");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        form.$(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
