459f440129d8916dc67a98436b8f58c3ef4a8f82
Vitaliy Rogov <vv55rogov@gmail.com>
2023-09-05 00:19:04 +0600

    test3

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -7,8 +7,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
     private String makeDate(int addDays, String pattern) {
@@ -19,11 +20,10 @@ public class CallbackTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Васильев Вася");
-        form.$("[data-test-id=phone] input").setValue("+79271234567");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        form.$(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=name] input").setValue("Васильев Вася");
+        $("[data-test-id=phone] input").setValue("+79271234567");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Продолжить")).click();
+        $(byText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")).shouldBe(visible);
     }
 }
